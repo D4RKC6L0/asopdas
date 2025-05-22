f32be9d062ee73f88933814ea32cda7f005d6040
MARCELO HENRIQUE SANTOS DA SILVA <marcelo_hs_silva@estudante.sesisenai.org.br>
2025-05-22 10:13:37 -0300

    wowowo

--- prova.js
+++ prova.js
@@ -9,3 +9,15 @@ server.use(express.json());
 server.options('/', (req, res) => {
     res.status(200).send({msg:'tudo ok'})
 })
+
+server.post('/receber_nome',(req,res) =>{
+    const nome=req.body.nome
+    if (nome){
+        registrar()
+        res.status(200).send({msg:'Nome recebido'})
+    }
+    else {
+        registrarf()
+        res.status(400).send({msg:'Nome não recebido'})
+    }
+    })
