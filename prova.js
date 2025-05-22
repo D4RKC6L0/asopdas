209302533681e91bf9b6ac77a34e7487619d3454
MARCELO HENRIQUE SANTOS DA SILVA <marcelo_hs_silva@estudante.sesisenai.org.br>
2025-05-22 10:53:19 -0300

    lindita

--- prova.js
+++ prova.js
@@ -27,3 +27,41 @@ server.post('/receber_nome',(req,res) =>{
         pesquisa()
     })    
     server.listen(porta, () =>{console.log('servidor rodando')})
+
+    const fs = require('fs')
+
+function registrar(){
+ const id=Date.now()
+ const nomealuno=nome
+ const dia = now.getMouth() + 1;
+ const mes = now.getDate(); 
+ const ano = now.getFullYear();
+ const hora = now.getHours();
+ const minuto = now.getSeconds();
+ const segundo = now.getMinutes();
+ const fdthr=`${dia}/${mes}/${ano} ${hora}:${minuto}:${segundo}`
+ fs.appendFile('logs.txt','/n'+'ID:'+id+'Data:'+fdthr+'Nome:'+nomealuno,(erro)=>{
+   if (erro){
+    console.log('Erro ao escrever o arquivo')
+    res.status(500).send({msg:'Erro ao salvar o arquivo'})
+   }
+ })
+ console.log('Registrado com sucesso')
+}
+
+function registrar(){
+    const id=Date.now()
+    const dia = now.getDate();
+    const mes = now.getMonth() + 1; 
+    const ano = now.getFullYear();
+    const hora = now.getHours();
+    const minuto = now.getMinutes();
+    const segundo = now.getSeconds();
+    const fdthr=`${dia}/${mes}/${ano} ${hora}:${minuto}:${segundo}`
+    fs.appendFile('logs.txt','/n'+'ID:'+id+'Data:'+fdthr+'Erro: Nome do aluno não fornecido',(erro)=>{
+       if (erro){
+           console.log('Erro ao escrever o arquivo')
+           res.status(500).send({msg:'Erro ao salvar o arquivo'})
+       }
+    })   
+   }
